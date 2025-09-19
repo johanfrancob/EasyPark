cecd45f39fba42f4a7158cd8cfbba2dbece49961
JOHAN CAMILO FRANCO BEDOYA <johanfranco323357@correo.itm.edu.co>
2025-09-19 18:04:50 -0500

    Se soluciona error de Mudblazor

--- EasyPark/EasyPark.Frontend/Program.cs
+++ EasyPark/EasyPark.Frontend/Program.cs
@@ -9,4 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 await builder.Build().RunAsync();
+
