f556673570df8312cf67fe88be26bef6d2d7badf
jaime-tovar <misterjto591@gmail.com>
2025-09-28 12:11:00 -0500

    Update habia controller, add principal page and edit nav menu

--- EasyPark/EasyPark.Backend/Controllers/BahiasController.cs
+++ EasyPark/EasyPark.Backend/Controllers/BahiasController.cs
@@ -58,38 +58,12 @@ namespace EasyPark.Backend.Controllers
             });
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<TblBahium>> Get(int id)
-        {
-            var bahia = await _context.TblBahia.FindAsync(id);
-            return bahia is null ? NotFound() : bahia;
-        }
-
         [HttpPost]
         public async Task<ActionResult<TblBahium>> Post(TblBahium bahia)
         {
             _context.TblBahia.Add(bahia);
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(Get), new { id = bahia.IdBahia }, bahia);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id, TblBahium bahia)
-        {
-            if (id != bahia.IdBahia) return BadRequest();
-            _context.Entry(bahia).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-            return NoContent();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var bahia = await _context.TblBahia.FindAsync(id);
-            if (bahia is null) return NotFound();
-            _context.TblBahia.Remove(bahia);
-            await _context.SaveChangesAsync();
-            return NoContent();
         }
     }
 }
